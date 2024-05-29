ed0947b185b565b9df6eb7dd0e41dbe62a7d35b3
amrmohamed <amoree.mb@gmail.com>
2024-05-29 16:31:57 +0300

    Create comment schema

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = Schema({
+  text: { type: String, minLength: 1, maxLength: 140, required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  created_at: { type: Date, default: Date.now() },
+});
