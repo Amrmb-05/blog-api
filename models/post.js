e8cc45d8ad2329156e1c4f5ea2acd37ac12b6118
amrmohamed <amoree.mb@gmail.com>
2024-05-29 16:34:38 +0300

    Add comments field for PostSchema

--- models/post.js
+++ models/post.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   title: { type: String, minLength: 1, required: true },
   text: { type: String, minLength: 1, maxLength: 280, required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment", required: true }],
   user: { type: Schema.Types.ObjectId, ref: "User" },
   created_at: { type: Date, default: Date.now() },
   public: { type: Boolean, default: true },
