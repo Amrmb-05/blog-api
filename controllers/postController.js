a209fd2c602f4bf745dbaff9b7bd8f5b30e91bff
amrmohamed <amoree.mb@gmail.com>
2024-05-30 13:41:08 +0300

    Create get route for single posts and a controller function for it

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,18 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 const asyncHandler = require("express-async-handler");
 
 exports.posts_list = asyncHandler(async (req, res, next) => {
   const posts = await Post.find({}).exec();
   res.json(posts);
+});
+
+exports.post_detail = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.id).exec();
+
+  if (post === null) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  res.json(post);
 });
--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 const post_controller = require("../controllers/postController");
 
+// Get All Posts
 router.get("/", post_controller.posts_list);
+
+// Get Single Post
+router.get("/:id", post_controller.post_detail);
