d66efd72a3e4472cf86f93d537bf02bb40828456
amrmohamed <amoree.mb@gmail.com>
2024-05-31 17:18:01 +0300

    Create log-in route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require("passport");
 const user_controller = require("../controllers/userController");
 
+// Sign Up
 router.post("/sign-up", user_controller.sign_up);
 
+// Log In
+router.post("/log-in", user_controller.log_in);
 module.exports = router;
