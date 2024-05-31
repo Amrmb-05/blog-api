e0caa5ec76790baad198a2f0cf5c8b62dbf8d50b
amrmohamed <amoree.mb@gmail.com>
2024-05-31 14:08:55 +0300

    make the user field not required until authentication is implemented

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const CommentSchema = Schema({
   text: { type: String, minLength: 1, maxLength: 140, required: true },
   post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
   created_at: { type: Date, default: Date.now() },
 });
 
